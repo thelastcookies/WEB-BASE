0e62cdbbee16322ce918e73c98e5c1299abf391f
thelastcookies <thelastcookies@icloud.com>
2025-09-15 15:08:45 +0800

    fix: 完成标签页对于组态首页的适配

--- src/views/layouts/components/Tabs.vue
+++ src/views/layouts/components/Tabs.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { RouteLocationNormalized, RouteRecordName } from 'vue-router';
+import type { LocationAsRelativeRaw, RouteLocationNormalized, RouteQueryAndHash, RouteRecordName } from 'vue-router';
 import type { Key } from '@/types';
 
 // TODO: 允许与不允许多开
@@ -16,14 +16,26 @@ const homepageId = import.meta.env.APP_HOMEPAGE_ID;
 const homepagePath = routes.find(r => r.name === homepageId)?.path!;
 
 const activePath = ref<string>(homepagePath);
-const tabs = ref<Tab[]>(routes.filter(r => r.name === homepageId)?.map(r => {
-  return {
-    name: r.name,
-    title: r.meta?.title as string,
-    path: r.path,
+const tabs = ref<Tab[]>([]);
+
+const initTab = () => {
+  const homeRoute = routes.find(r => r.name === homepageId)!;
+
+  let path = homeRoute.path;
+  if (homeRoute.redirect && (homeRoute.redirect as LocationAsRelativeRaw)['name'] === 'DIAGRAM') {
+    path = routes.find(r => r.name === 'DIAGRAM')!.path + '?d=' + (homeRoute.redirect as RouteQueryAndHash).query!.d;
+  }
+  const homeTab = {
+    name: homeRoute.name,
+    title: homeRoute.meta?.title as string,
+    path,
     closable: false,
   };
-}));
+  activePath.value = path!;
+  tabs.value.push(homeTab);
+};
+
+initTab();
 
 const updateTab = (tab: Tab) => {
   activePath.value = tab.path!;

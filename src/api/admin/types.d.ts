59f8ed9b8ab47917054e037fbab6f3f215ee988a
thelastcookies <thelastcookies@icloud.com>
2025-10-25 21:29:43 +0800

    type: 补充 Admin 接口类型声明

--- src/api/admin/types.d.ts
+++ src/api/admin/types.d.ts
@@ -14,3 +14,54 @@ export interface RequestPagination {
   Page?: number;
   Limit?: number;
 }
+
+export interface GetDataByIdRequestBody {
+  Ids?: string;
+}
+
+export interface GetDataByStationIdRequestBody {
+  SIds?: string;
+}
+
+export interface GetDataByTransformerIdRequestBody {
+  TIds?: string;
+}
+
+export interface GetDataByInverterIdRequestBody {
+  IIds?: string;
+}
+
+export interface GetDataByLineRequestBody {
+  Lines?: string;
+}
+
+export interface GetDataRealtimeRequestBody {
+  RT?: '1' | '0';
+}
+
+export interface GetPointDataShowTypeRequestBody {
+  ShowType?: string;
+}
+
+export type GetPointInfoRequestBody =
+  GetDataByIdRequestBody
+  | GetDataByStationIdRequestBody
+  | GetPointDataShowTypeRequestBody;
+
+export type GetPointInterpRequestBody = GetDataByIdRequestBody | {
+  Index?: string;
+  // 时间格式: "YYYYMMDDHHMMSS-YYYYMMDDHHMMSS"
+  Time?: string;
+  Interval?: number;
+}
+
+export interface PointInfoRecord {
+  Id: string;
+  Name: string;
+  Tag: string;
+  TagType: 'AI' | 'DI';
+  Value: string;
+  Unit: string;
+  Order: string;
+  ShowType: '列表' | '表格' | '仪表盘' | '光字牌';
+}
